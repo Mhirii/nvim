cd0ca2919dae83f636eb7459f1beac72491a7b71
Ahmed Mhiri <ahmedmhiri218@gmail.com>
2023-08-13 16:16:49 +0100

    NvChad

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,57 @@
+local plugins = {
+  {"elkowar/yuck.vim", lazy = false},
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "rust-analyzer",
+      }
+    }
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function ()
+      return require "custom.configs.rust-tools"
+    end,
+    config = function(_, opts)
+      require('rust-tools').setup(opts)
+    end
+  },
+  {
+    "mfussenegger/nvim-dap",
+  },
+  {
+    'saecki/crates.nvim',
+    ft = {"toml"},
+    config = function (_, opts)
+      local crates = require("crates")
+      crates.setup(opts)
+      crates.show()
+    end
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function ()
+      local M = require "plugins.configs.cmp"
+      table.insert(M.sources, {name = "crates"})
+      return M
+    end
+  }
+}
+return plugins
